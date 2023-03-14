99508663f0c97b9e181ad281128a1b19a2f6e90f
Alexander Weers <mail@aweers.de>
2023-03-14 16:36:49 +0100

    Initial commit

--- main.ts
+++ main.ts
@@ -1,81 +1,44 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Notice, Plugin, PluginSettingTab, Setting, requestUrl } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface PaperNotesSettings {
+	template: string;
+	authors_as_link: boolean;
+	file_name_format: string;
+	folder: string;
+	pdf_folder: string;
+	download_pdf: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: PaperNotesSettings = {
+	template: '# {{title}}\nAuthors: {{authors}}\nLink: [PDF]({{pdf_file}})\n\n## Abstract\n{{abstract}}',
+	authors_as_link: true,
+	file_name_format: 'title',
+	folder: 'papers',
+	pdf_folder: '_pdfs',
+	download_pdf: true
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class PaperNotesPlugin extends Plugin {
+	settings: PaperNotesSettings;
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		this.addRibbonIcon('newspaper', 'Paper notes', (evt: MouseEvent) => {
+			new PaperNotesModal(this).open();
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: 'open-paper-notes-modal',
+			name: 'Load paper details',
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				new PaperNotesModal(this).open();
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new PaperNotesSettingsTab(this.app, this));
 	}
 
 	onunload() {
@@ -89,16 +52,149 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async loadPaperDetails(paper_id: string) {
+		const base_url = 'https://arxiv.org/';
+		const url = base_url + 'abs/' + paper_id;
+		const pdf_url = base_url + 'pdf/' + paper_id + '.pdf';
+
+		const response = await requestUrl(url);
+		const text = await response.text;
+
+		const parser = new DOMParser();
+		const doc = parser.parseFromString(text, "text/html");
+
+		const abs = doc.querySelector('#abs');
+		let title = abs?.querySelector('.title')?.textContent || 'No title';
+		let authors = abs?.querySelector('.authors')?.textContent;
+		let abstract = abs?.querySelector('.abstract')?.textContent;
+
+		if (title) {
+			title = title.replace('Title:', '');
+		}
+
+		if (abstract) {
+			abstract = abstract.replace('Abstract: ', '');
+			abstract = abstract.replace(/\n/g, ' ');
+			abstract = abstract.replace('  ', ' ');
+			abstract = abstract.replace(/\t/g, '');
+
+			abstract = abstract.trim();
+		}
+
+		// Convert authors to links
+		if (authors && this.settings.authors_as_link) {
+			const authors_list = authors.split(', ');
+			for (let i = 0; i < authors_list.length; i++) {
+				if (i == 0) {
+					authors_list[i] = authors_list[i].replace('Authors:', '');
+				}
+				authors_list[i] = '[[' + authors_list[i] + ']]';
+			}
+			authors = authors_list.join(', ');
+		}
+
+		// Add paper details
+		let note = this.settings.template;
+		
+		// Insert data into template
+		if (title) note = note.replace('{{title}}', title);
+		if (authors) note = note.replace('{{authors}}', authors);
+		if (abstract) note = note.replace('{{abstract}}', abstract);
+
+		note = note.replace('{{pdf_file}}', this.settings.folder + '/' + this.settings.pdf_folder + '/' + paper_id + '.pdf');
+
+		if (this.settings.download_pdf) {
+			if (!this.app.vault.getAbstractFileByPath(this.settings.folder + '/' + this.settings.pdf_folder)) {
+				await this.app.vault.createFolder(this.settings.folder + '/' + this.settings.pdf_folder);
+			}
+
+			if (this.app.vault.getAbstractFileByPath(this.settings.folder + '/' + this.settings.pdf_folder + '/' + paper_id + '.pdf')) {
+				new Notice('PDF already exists: ' + paper_id);
+			}
+			else {
+				const pdf_response = await requestUrl(pdf_url);
+				const pdf_blob = await pdf_response.arrayBuffer;
+
+				await this.app.vault.createBinary(this.settings.folder + '/' + this.settings.pdf_folder + '/' + paper_id + '.pdf', pdf_blob);
+			}
+		}
+
+		// Create new note in correct folder
+		let paper_title = "";
+		if (this.settings.file_name_format == 'title') {
+			paper_title = title.replace(':', '-');
+		}
+		else if (this.settings.file_name_format == 'id') {
+			paper_title = paper_id;
+		}
+		const file_path = this.settings.folder + '/' + paper_title + '.md';
+
+		if (this.app.vault.getAbstractFileByPath(file_path)) {
+			new Notice('File already exists');
+			return;
+		}
+
+		if (!this.app.vault.getAbstractFileByPath(this.settings.folder)) {
+			await this.app.vault.createFolder(this.settings.folder);
+		}
+		
+		const file = await app.vault.create(file_path, note);
+
+		app.workspace.getMostRecentLeaf()?.openFile(file);
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
+class PaperNotesModal extends Modal {
+	plugin: PaperNotesPlugin;
+
+	constructor(plugin: PaperNotesPlugin) {
+		super(plugin.app);
+		this.plugin = plugin;
 	}
 
 	onOpen() {
 		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		
+		const inputContainer = contentEl.createDiv('input-container');
+		inputContainer.style.display = 'flex';
+		inputContainer.style.justifyContent = 'flex-start';
+
+		inputContainer.createEl('label', {
+			text: 'Paper ID or URL: '
+		});
+
+		const input = inputContainer.createEl('input', {
+			type: 'text',
+			placeholder: 'Paper ID or URL'
+		});
+
+		input.style.width = '60%';
+		input.style.marginLeft = '10px';
+
+		// Create button in new row
+		const buttonContainer = contentEl.createDiv('button-container');
+		buttonContainer.style.display = 'flex';
+		buttonContainer.style.justifyContent = 'flex-end';
+
+		const button = buttonContainer.createEl('button', {
+			text: 'Load paper details',
+			cls: 'mod-cta',
+
+		});
+
+		// Add event listener to button
+		button.addEventListener('click', () => {
+			let paper_id = input.value;
+
+			paper_id = paper_id.replace('https://arxiv.org/abs/', '');
+			paper_id = paper_id.replace('https://arxiv.org/pdf/', '');
+			paper_id = paper_id.replace('.pdf', '');
+			paper_id = paper_id.replace('.html', '');
+
+			this.plugin.loadPaperDetails(paper_id);
+			this.close();
+		});
 	}
 
 	onClose() {
@@ -107,10 +203,10 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class PaperNotesSettingsTab extends PluginSettingTab {
+	plugin: PaperNotesPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: PaperNotesPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,18 +216,97 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for paper notes plugin.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+			.setName('Template')
+			.setDesc('Use this template to create new notes. Use {{title}}, {{authors}}, {{pdf_file}} and {{abstract}} as placeholders.')
+			.addTextArea(text => {
+				text
+				.setPlaceholder("Template")
+				.setValue(this.plugin.settings.template)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.template = value;
 					await this.plugin.saveSettings();
-				}));
+				});
+				text.inputEl.rows = 15;
+				text.inputEl.cols = 50;
+			});
+		
+		// Checkbox to toggle authors as links
+		new Setting(containerEl)
+			.setName('Authors as links')
+			.setDesc('Convert authors to links.')
+			.addToggle(toggle => {
+				toggle
+				.setValue(this.plugin.settings.authors_as_link)
+				.onChange(async (value) => {
+					this.plugin.settings.authors_as_link = value;
+					await this.plugin.saveSettings();
+				});
+			}
+		);
+
+		// Dropdown to select file name format
+		new Setting(containerEl)
+			.setName('File name format')
+			.setDesc('Select the format for the file name.')
+			.addDropdown(dropdown => {
+				dropdown
+				.addOption('title', 'Paper title')
+				.addOption('id', 'Paper ID')
+				.setValue(this.plugin.settings.file_name_format)
+				.onChange(async (value) => {
+					this.plugin.settings.file_name_format = value;
+					await this.plugin.saveSettings();
+				});
+			}
+		);
+
+		// Text input to specify folder for notes
+		new Setting(containerEl)
+			.setName('Folder for notes')
+			.setDesc('Specify the folder for notes. Default is "papers".')
+			.addText(text => {
+				text
+				.setPlaceholder("papers")
+				.setValue(this.plugin.settings.folder)
+				.onChange(async (value) => {
+					if (value == '') value = 'papers';
+					this.plugin.settings.folder = value;
+					await this.plugin.saveSettings();
+				});
+			}
+		);
+
+		// Checkbox to toggle download PDF
+		new Setting(containerEl)
+			.setName('Download PDF')
+			.setDesc('Download PDF and save in some folder.')
+			.addToggle(toggle => {
+				toggle
+				.setValue(this.plugin.settings.download_pdf)
+				.onChange(async (value) => {
+					this.plugin.settings.download_pdf = value;
+					await this.plugin.saveSettings();
+				});
+			}
+		);
+
+		// Text input to specify folder for PDFs
+		new Setting(containerEl)
+			.setName('Folder for PDFs')
+			.setDesc('Specify the folder for PDFs. Default is "_pdfs".')
+			.addText(text => {
+				text
+				.setPlaceholder("pdfs")
+				.setValue(this.plugin.settings.pdf_folder)
+				.onChange(async (value) => {
+					if (value == '') value = '_pdfs';
+					this.plugin.settings.pdf_folder = value;
+					await this.plugin.saveSettings();
+				});
+			}
+		);
 	}
 }
